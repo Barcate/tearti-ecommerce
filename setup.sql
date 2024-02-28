ebf2602219d644ac5f26fbd8238d0acae6f8eeb8
joojdev <96006422+joojdev@users.noreply.github.com>
2024-02-28 08:02:08 -0300

    Update setup.sql

--- setup.sql
+++ setup.sql
@@ -3,17 +3,17 @@ create database tear;
 use tear;
 
 create table produto (
-	id int auto_increment,
-	nome varchar(100),
+    id int auto_increment,
+    nome varchar(100),
     descricao varchar(500),
     preco numeric,
-	estoque int,
-	disponivel bit,
-	primary key(id)
+    estoque int,
+    disponivel bit,
+    primary key(id)
 );
 
 create table thumbnail (
-	id int auto_increment,
+    id int auto_increment,
     id_produto int,
     caminho_imagem text,
     foreign key(id_produto) references produto(id),
