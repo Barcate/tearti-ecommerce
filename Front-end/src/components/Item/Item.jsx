0d1d95fc306efcaf0795c9ebd0ef42ed485b2981
Barcate <mateusbosa21@gmail.com>
2024-06-27 16:56:30 -0300

    Interface funcional e buscando os dados do produto

--- Front-end/src/components/Item/Item.jsx
+++ Front-end/src/components/Item/Item.jsx
@@ -9,7 +9,6 @@ const Item = (props) => {
               <div className="imagem"><img src = {props.imagem} /></div>
               <div className="informacao">
                 <p className="nome">{props.name}</p>
-                <p className="key">{props.key}</p>
                 <p className="preco">{props.price}</p>
                 <p className="estoque">{props.estoque}</p>
                 <p className="disponivel">{props.disponivel}</p>
